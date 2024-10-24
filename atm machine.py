64b4ba89794ec161e0037f0366ac429e1856d7f9
Samarth N <92016409+Samarth-N@users.noreply.github.com>
2024-10-24 23:10:38 +0530

    Update atm machine.py

--- atm machine.py
+++ atm machine.py
@@ -2,24 +2,20 @@
 import time
 
 print("Please insert Your CARD")
-
-#for card processing
 time.sleep(5)
 
 password = 1234
-
-#taking atm pin from user
 pin = int(input("enter your atm pin "))
 
-#user account balance
+
 balance = 5000
 
-#checking pin is valid or not 
+
 if pin == password:
-    #loop will run user get free 
+    
     while True:
 
-        #Showing  info to user
+      
 
         print(""" 
 			1 == balance
@@ -30,12 +26,12 @@ if pin == password:
               )
 
         try:    
-             #taking an option from user
+             
             option = int(input("Please enter your choise "))
         except:
             print("Please enter valid option")
         
-        #for option 1        
+               
         if option == 1:
             print(f"Your current balance is {balance}")
                                      
